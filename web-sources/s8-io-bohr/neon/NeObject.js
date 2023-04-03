b1aa7919a505da04d194befe23554251ac46d837
Pierre Convert <pierreconvert@macbook-pro-6.lan>
2023-04-03 18:32:24 +0200

    adding js test

---
+++ web-sources/s8-io-bohr/neon/NeObject.js
@@ -0,0 +1,22 @@
+import { NeVertex } from "./NeVertex.js";
+
+
+
+export class NeObject {
+
+
+    /**
+     * Automatically assigned by NeObjectTypeHandler
+     * 
+     * @type {NeVertex}
+     */
+    S8_vertex;
+
+
+    constructor() {
+    }
+
+
+    
+
+}
