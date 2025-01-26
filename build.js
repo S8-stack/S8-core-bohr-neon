a0b0321d591cf1e73d8dc5c4d316b1ec2c5e0395
Pierre Convert <pierreconvert@192.168.1.14>
2025-01-26 17:10:56 +0100

    Updating build.js

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const ROOT = build({
 	module: "com.s8.core.bohr.neon",
 	dependencies: [ "S8-api", "S8-core-io-bytes", "S8-core-bohr-atom"],
-	target: "S8-core-bohr-neon",
+	target: "S8-core-bohr-neon"
 });
